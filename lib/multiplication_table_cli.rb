7d3a90e8dcc0d4084ec3ceb518d0c26022fbc904
Anuj Biyani <anujbiyani01@gmail.com>
2015-06-02 08:04:51 -0700

    add lib to the load path for easier requires

--- lib/multiplication_table_cli.rb
+++ lib/multiplication_table_cli.rb
@@ -9,7 +9,7 @@ class MultiplicationTableCli
     OptionParser.new do |opts|
       opts.banner = 'Usage: ./bin/multiplication_table [options]'
 
-      opts.on('-c COUNT', '--count COUNT', Integer, 'Number of primes for which a table will be built') do |count|
+      opts.on('-c COUNT', '--count COUNT', Integer, 'Number of primes for which a table will be built (default = 10)') do |count|
         options[:count] = count
       end
     end.parse!(argv)
